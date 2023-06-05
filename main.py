eed067408d1ba4aea6a152f128e4e9c5b920152e
unknown <vinicius.ferreira.bezerra07@aluno.ifce.edu.br>
2023-06-05 19:10:09 -0300

    Base de dados importada

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import numpy as np
 import pandas as pd
-
+import streamlit as st
 
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
-print("a")
+import seaborn as sns
+
+
+#Se baixar a base de dados para usar remotamente, alterar o caminho para o local onde esta a base baixada
+#https://www.kaggle.com/datasets/suraj520/music-dataset-song-information-and-lyrics?resource=download
+dataBase = pd.read_csv("songs.csv")
+
+# dataBase = pd.read_csv("/kaggle/input/music-dataset-song-information-and-lyrics/songs.csv")
+
+sns.histplot(dataBase, x='Popularity', kde=True, color='g')
