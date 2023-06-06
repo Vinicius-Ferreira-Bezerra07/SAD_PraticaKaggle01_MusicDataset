ab1a83ebc9bc33d3605786e2f811ca74de910496
Vinicius Ferreira Bezerra <60759024+Vinicius-Ferreira-Bezerra@users.noreply.github.com>
2023-06-06 15:51:20 -0300

    tentativa de rodar o grafico de barras / problema com running do streamlit no PC

--- main.py
+++ main.py
@@ -8,21 +8,26 @@ import streamlit as st
 # from sklearn.metrics.pairwise import cosine_similarity
 # from sklearn.feature_extraction.text import CountVectorizer
 
-#Se baixar a base de dados para usar remotamente, alterar o caminho para o local onde esta a base baixada
-#https://www.kaggle.com/datasets/suraj520/music-dataset-song-information-and-lyrics?resource=download
+# Se baixar a base de dados para usar remotamente, alterar o caminho para o local onde esta a base baixada
+# https://www.kaggle.com/datasets/suraj520/music-dataset-song-information-and-lyrics?resource=download
 
 dataBase = pd.read_csv("songs.csv")
 
-#informações da database
+# informações da database
 # print(dataBase.info())
 # print(dataBase.describe())
 
 st.title("DataBase Songs - Streamlit")
 
-#dataBase = pd.read_csv("/kaggle/input/music-dataset-song-information-and-lyrics/songs.csv")
+# dataBase = pd.read_csv("/kaggle/input/music-dataset-song-information-and-lyrics/songs.csv")
 
 # Grafico de Barras de musicas/generos
 st.subheader("Distribuição de musicas por genero")
-music_count = dataBase['Name'].value_counts()
-print(music_count)
-sns.barplot(x=music_count.index, y=music_count.values)
+popularidade_artista = dataBase[['Artist', 'Popularity']]
+popularidade_artista = popularidade_artista.groupby('Artist').sum()
+print(popularidade_artista)
+fig, ax = plt.subplots()
+sns.barplot(x=popularidade_artista.index,
+            y=popularidade_artista['Popularity'])
+
+st.pyplot(fig)
